cd7be0af9a40ccd66a60e12d8efeb91b4d3e70af
jmdgab75 <gabrieljmd75@gmail.com>
2023-08-29 17:34:39 -0400

    commit localStorage

--- src/app/pages/dashboard/page.js
+++ src/app/pages/dashboard/page.js
@@ -1,6 +1,7 @@
 import { getUsers } from "@/app/functions/handlerAcessAPI";
 import { Suspense } from "react";
 import  ListUser from "@/app/functions/componentes/ListUser.js";
+//import ".."
  
 export default async function Dashboard() {
    const users = await getUsers();
--- src/middleware.js
+++ src/middleware.js
@@ -1,9 +1,7 @@
-'use server'
 import { NextResponse } from "next/server";
 import { validateToken } from "./app/functions/validateToken";
 
 export const middleware = (request) => {
-
     const token = request.cookies.get('token')?.value;
     const urlLogin = new URL('/', request.url);
     const isTokenValidated = validateToken(token);
@@ -17,12 +15,15 @@ export const middleware = (request) => {
 
     if (isTokenValidated) {
         if (request.nextUrl.pathname === '/') {
+            const userName = "John Doe"; // Replace this with the actual user's name
+            localStorage.setItem("userName", userName); // Store user's name in localStorage
             return NextResponse.redirect(urlDashboard);
         }
     }
+    
     NextResponse.next();
 };
+
 export const config = {
     matcher: ['/', '/pages/dashboard']
 };
-
