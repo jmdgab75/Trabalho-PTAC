2920185bbfb544c1af1bbe2221eb8fc70b109727
jmdgab75 <gabrieljmd75@gmail.com>
2023-10-17 17:37:02 -0400

    commit 17/10

--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -1,53 +1,22 @@
 'use server'
 
-const getUser = [
-    {
-      name: "Gabriel",
-      email: "gabriel@email.com",
-      password: "123",
-      token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
-    },
-    {
-      name: "Rose",
-      email: "rose@email.com",
-      password: "1234",
-      token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
-    },
-    {
-      name: "Maria",
-      email: "Maria@email.com",
-      password: "246",
-      token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
-    },
-    {
-      name: "João",
-      email: "joao@email.com",
-      password: "135",
-      token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
-    },
-    {
-      name: "Rafa",
-      email: "rafa@email.com",
-      password: "012",
-      token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
-    }
-  ];
 
-  
-const getUserAuthenticated = (userLogin) => {
-   let usermach = {};
+const url = "https://aula-17-10-marcelino-trabalho.vercel.app/";
 
-    getUser.map((user) =>{
-
-   if(user.email === userLogin.email && user.password === userLogin.password){
-   usermach = user
-    } 
-})
- 
-return usermach
+const getUserAuthenticated = async (user) => {
+   const responseOfApi = await fetch(url + "/users/authenticaded",
+     {
+      method:"POST",
+      headers:{ "Content-Type":"Application/json" },
+      body: JSON.stringify(user)
+     }
+   );
+   const userAuth = await responseOfApi.json();
+   return userAuth;
 }
+  
 
 const getUsers = () =>{
-     return getUser
+    
 }
 export { getUsers, getUserAuthenticated };
